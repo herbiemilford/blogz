3df69ebb0e7dac55b8f0d70c278a0157f1aecba9
Joe Crowder <toast.one.art@gmail.com>
2018-11-14 07:43:40 -0600

    deleted img add templates index login signup singleuser, completed signup template and main handler

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from sqlalchemy import desc
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI']='mysql+pymysql://build-a-blog:beproductive@localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_DATABASE_URI']='mysql+pymysql://blogz:beproductive@localhost:8889/blogz'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 
@@ -39,6 +39,61 @@ class Blog(db.Model):
 @app.route('/')
 def index():
     return redirect('/blog')
+
+#signup route-validate/verify 
+
+@app.route("/signup", methods=['POST', 'GET'])
+def signup():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        verify = request.form['verify']
+        exist = User.query.filter_by(username=username).first()
+
+        username_error = ""
+        password_error = ""
+        verify_error = ""
+
+        if username == "":
+            username_error = "Please enter a username."
+        elif len(username) <= 3 or len(username) > 20:
+            username_error = "Username must be between 3 and 20 characters long."
+        elif " " in username:
+            username_error = "Username cannot contain any spaces."
+        if password == "":
+            password_error = "Please enter a password."
+        elif len(password) <= 3:
+            password_error = "Password must be greater than 3 characters long."
+        elif " " in password:
+            password_error = "Password cannot contain any spaces."
+        if password != verify or verify == "":
+            verify_error = "Passwords do not match."
+        if exist:
+            username_error = "Username already taken."
+        # If fields are good, continue to creating session with new username and password.
+        if len(username) > 3 and len(password) > 3 and password == verify and not exist:
+            new_user = User(username, password)
+            db.session.add(new_user)
+            db.session.commit()
+            session['username'] = username
+            return redirect('/newpost')
+        else:
+            return render_template('signup.html',
+            username=username,
+            username_error=username_error,
+            password_error=password_error,
+            verify_error=verify_error
+            )
+
+    return render_template('signup.html')
+
+#login route-validate/verify user info in database
+
+@app.route('/login', methods=['POST', 'GET'])
+
+
+#index route-redirects to home
+
 
 
 #route to main blog page
